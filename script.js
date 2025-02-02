d2d00e6e40d71c51e45131e6cadaf0a32a1be44e
NanoBinary <nanobinary01@gmail.com>
2025-02-02 17:09:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  const navMenu = document.getElementById('navMenu');
-  const menuIcon = document.querySelector('.nav_line_container');
-  navMenu.classList.toggle('active');
-  menuIcon.classList.toggle('active');
-}
